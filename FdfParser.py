7154fbd65b8196a3f8a5098be0b9b74432c4d936
ren.yang <goyour123@gmail.com>
2018-05-15 15:27:31 +0800

    Enhance the algorithm of calculating macros

--- FdfParser.py
+++ FdfParser.py
@@ -6,33 +6,32 @@ def get_region_value(val, macro_dict):
     try:
         int(val, base=16)
     except:
-        macro = re.findall(r'\$\((.*)\)', val)
+        macro = re.findall(r'\$\((\S*)\)', val)
         val = macro_dict[macro[0]]
     else:
         pass
 
     return hex(int(val, base=16))
 
-def update_macro_dict(key, val, dict):
-    try:
-        int(val, base=16)
-    except:
-        oprd = re.findall(r'\$\((.*?)\)', val)
-        operator = re.findall(r'([\+\-\*/])', val)
-        result = int(dict[oprd[0]], base=16)
+def update_macro_dict(key, line, dict):
+    oprd = re.findall(r'\s*[\+\-\*/=]\s*([^\+\-\*\/\n\s]+)', line)
+    operator = re.findall(r'([\+\-\*/])', line)
+    
+    result = int(get_region_value(oprd[0], dict), base=16)
+
+    if len(operator) > 0:
         for idx, optr in enumerate(operator):
+            val = int(get_region_value(oprd[idx + 1], dict), base=16)
             if (optr == '+'):
-                result += int(dict[oprd[idx + 1]], base=16)
+                result += val
             elif (optr == '-'):
-                result -= int(dict[oprd[idx + 1]], base=16)
+                result -= val
             elif (optr == '*'):
-                result *= int(dict[oprd[idx + 1]], base=16)
+                result *= val
             elif (optr == '/'):
-                result /= int(dict[oprd[idx + 1]], base=16)
-        val = hex(int(result))
-    else:
-        pass
-    dict[key] = val
+                result /= val
+
+    dict[key] = hex(int(result))
     return dict
 
 def main():
@@ -43,9 +42,9 @@ def main():
     with open(sys.argv[1], 'r') as f:
         for line in f:
             sect = re.findall(r'\[FD\.(.+)\]', line)
-            macro = re.findall(r'DEFINE\s+?(\S+)\s+=\s+?(\S+)\s*', line)
+            macro = re.findall(r'DEFINE\s+?(\S+)\s*\S', line)
             if len(fd_list) > 0:
-                region = re.findall(r'([$0].+)\|([$0].+)', line)
+                region = re.findall(r'([\$0].+)\|([\$0].+)', line)
                 if len(region) > 0:
                     fd_info[fd_list[fd_count-1]].append(region[0])
             if len(sect) > 0:
@@ -54,7 +53,7 @@ def main():
                 fd_info[fd_list[fd_count-1]] = []
             if len(macro) > 0:
                 # Collect MACROs into a dict
-                macro_dict = update_macro_dict(macro[0][0], macro[0][1], macro_dict)
+                macro_dict = update_macro_dict(macro[0], line, macro_dict)
 
     # Output the MACRO dict as a JSON file
     macro_json = json.JSONEncoder().encode(macro_dict)
@@ -70,5 +69,6 @@ def main():
                 get_region_value(region_size, macro_dict)
                 f.writelines(region_offect + '|' + region_size + ' ' + get_region_value(region_offect, macro_dict) + '|' + get_region_value(region_size, macro_dict) +'\n')
             f.writelines('\n')
+
 if __name__ == '__main__':
     main()
