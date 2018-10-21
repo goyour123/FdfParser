fb653a701e28a7893c0395ca7adc3df6ef623d3d
ren.yang <goyour123@gmail.com>
2018-10-21 23:23:09 +0800

    Pack FdVisualizer v3.0 into execution file

--- build-setup_cxfreeze.bat
+++ build-setup_cxfreeze.bat
@@ -1,3 +1,4 @@
-@set PYTHON3_PATH=C:\Users\Ren.Yang\AppData\Local\Programs\Python\Python36-32
-%PYTHON3_PATH%\python.exe .\setup_cxfreeze.py build
+@set PYTHON3_PATH="C:\Program Files (x86)\Python36-32\python.exe"
+@rmdir %cd%\build /s /q
+%PYTHON3_PATH% .\setup_cxfreeze.py build
 @pause
--- setup_cxfreeze.py
+++ setup_cxfreeze.py
@@ -1,7 +1,7 @@
 import os, sys
 from cx_Freeze import setup, Executable
 
-python3_path = r'C:\Users\Ren.Yang\AppData\Local\Programs\Python\Python36-32'
+python3_path = r'C:\Program Files (x86)\Python36-32'
 
 os.environ['TCL_LIBRARY'] = python3_path + r'\tcl\tcl8.6'
 os.environ['TK_LIBRARY'] = python3_path + r'\tcl\tk8.6'
@@ -21,7 +21,7 @@ executables = [
 ]
 
 setup(name='FdVisualizer',
-      version='2.3',
+      version='3.0',
       description='Visualize fdf layout',
       executables=executables,
       options=options
