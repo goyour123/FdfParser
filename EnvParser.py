6ca48c5c6f9d222dd05bc675870d0203094a7298
Ren Yang <ren.yang@insyde.com>
2022-01-17 13:49:42 +0800

    Add argument parser for pcd parser

--- EnvParser.py
+++ EnvParser.py
@@ -1,5 +1,6 @@
 import sys, os
 import re, json
+import argparse
 from warnings import warn
 
 def dict2JsonFile(jsonFilePath, srcDict):
@@ -15,28 +16,40 @@ def parseEnv(config_dict):
           continue
       else:
           line = line.split('#')[0]
-      
+
       defi = re.findall(r'.*DEFINE\s+(\S+)\s+=\s+(\S+)\s*', line)
       if defi:
         defiDict.update({defi[0][0]: defi[0][1]})
-      
+
       defi2 = re.findall(r'.*EDK_GLOBAL\s+(\S+)\s+=\s+(\S+)\s*', line)
       if defi2:
         defiDict.update({defi2[0][0]: defi2[0][1]})
   return defiDict
 
+def parsePcdList():
+  pass
+
 if __name__ == '__main__':
 
-  arg_names = ['script', 'env_file']
-  args = dict(zip(arg_names, sys.argv))
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-e", "--EnvVarFile", type=lambda p: str(os.path.abspath(p)), help="Environment variable file")
+  parser.add_argument("-p", "--PcdFile", type=lambda p: str(os.path.abspath(p)), help="PCD value file")
 
-  if len(args) > 1:
-    if os.path.isfile(args['env_file']):
-      envDict = parseEnv({'Env': args['env_file']})
+  args = parser.parse_args()
+
+  if args.EnvVarFile:
+    if os.path.isfile(args.EnvVarFile):
+      envDict = parseEnv({'Env': args.EnvVarFile})
     else:
-      warn('System argument 1 is not a file path')
-  else:
-    pass
+      warn('--EnvVarFile is not a file path')
+      sys.exit()
+
+  if args.PcdFile:
+    if os.path.isfile(args.PcdFile):
+      envDict = parsePcdList({'PcdList': args.PcdFile})
+    else:
+      warn('--PcdFile is not a file path')
+      sys.exit()
 
   if envDict:
     dict2JsonFile('Env.json', envDict)
